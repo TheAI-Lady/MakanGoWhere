b40133345c88fc030292f5bdedcd9595d42b485f
TheAI-Lady <39267943+TheAI-Lady@users.noreply.github.com>
2021-01-10 16:14:36 +0800

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var host = "127.0.0.1";
 var port = 8080;
 var startPage = "index.html";
 
-app.use(express.static("./public"));
+app.use(express.static("./docs"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -31,5 +31,5 @@ var server = app.listen(port, host, function() {
     var host = server.address().address;
     var port = server.address().port;
 
-    console.log("Example app listening at http://%s:%s", host, port);
+    console.log("MakanGoWhere app listening at http://%s:%s", host, port);
 });
